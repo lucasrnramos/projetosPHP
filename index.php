67cbbb40c40f86eb3606a243e772ddbc47b35db1
Lucas Ramos <84045235+lucasrnramos@users.noreply.github.com>
2022-04-17 12:19:33 -0300

    Alteração de tipo do arquivo.
    Criação de array e algumas variáveis;

--- index.php
+++ index.php
@@ -1,10 +1,17 @@
+<?php
+    $_vEmployeeOne    = 'Lucas';
+    $_vEmployeeTwo    = 'Josué';
+    $_vEemployeeThree = 'Eduardo';
+    $_vEmployees = array($_vEmployeeOne, $_vEmployeeTwo, $_vEemployeeThree);
+?>
+
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CrochêArt</title>
+    <title>Praticando</title>
 </head>
 <body>
     
