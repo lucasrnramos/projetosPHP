bf064a88c7678126fb6f6a152f7f0df719dc7bc0
Lucas Ramos <84045235+lucasrnramos@users.noreply.github.com>
2022-04-18 16:24:27 -0300

    index alterado

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
     $_vEmployeeOne    = 'Lucas';
     $_vEmployeeTwo    = 'Josué';
     $_vEemployeeThree = 'Eduardo';
-    $_vEmployees = array($_vEmployeeOne, $_vEmployeeTwo, $_vEemployeeThree);
+   // $_vEmployees = array($_vEmployeeOne, $_vEmployeeTwo, $_vEemployeeThree);
+    $_vSalary = array(3290, 900, 6000);
 ?>
 
 <!DOCTYPE html>
@@ -11,9 +12,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Praticando</title>
+    <title>Praticando</title>   
 </head>
 <body>
-    
+    <pre>
+    <?php
+        print_r($_vEmployees[0] . $_vSalary[0]);
+    ?>
+    </pre>
 </body>
 </html>
